c6a60e44cae2edaca89a76cd84eebffd9e72f7d5
ewakasprzak <ewa.j.kasprzak@gmail.com>
2023-05-27 16:43:10 +0200

    first commit

---
+++ src/sea/App.java
@@ -0,0 +1,19 @@
+package sea;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception 
+    {
+        
+    }
+
+}
